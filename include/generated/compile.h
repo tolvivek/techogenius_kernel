39bf9960ede933acb5627c027cfe2dc2c6de1501
VIVEK TOL <tolvivek@gmail.com>
2023-11-20 22:26:29 +0530

    vivek added techogenious course kernel on my own git

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP Sun May 7 22:36:37 IST 2023"
+#define LINUX_COMPILE_BY "vivek"
+#define LINUX_COMPILE_HOST "vivek-linux"
+#define LINUX_COMPILER "gcc version 8.3.0 (GNU Toolchain for the A-profile Architecture 8.3-2019.03 (arm-rel-8.36))"
